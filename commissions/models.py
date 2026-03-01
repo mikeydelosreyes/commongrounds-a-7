dfa89c18588127f7f2292a97e03acbc662ce2ab0
pierce-06 <pierce.johan.cabalteja@student.ateneo.edu>
2026-03-01 23:09:05 +0800

    Added CommissionType model

--- commissions/models.py
+++ commissions/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import datetime
+from django.urls import *
 
-# Create your models here.
+class CommissionType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('commissiontype_list', args=[str(self.name)])
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'commission type'
+        verbose_name_plural = 'commission types'
+
