9ae17b884a9d7643813e1a4ad50e9e5e6596f474
pierce-06 <pierce.johan.cabalteja@student.ateneo.edu>
2026-03-02 00:40:59 +0800

    Removed the InlineAdmin for CommissionType

--- commissions/admin.py
+++ commissions/admin.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
 from .models import *
 
-class CommissionInline(admin.TabularInline):
-    model = Commission
-
-
 class CommissionTypeAdmin(admin.ModelAdmin):
     model = CommissionType
-    inlines = [CommissionInline, ]
 
 
 class CommissionAdmin(admin.ModelAdmin):
