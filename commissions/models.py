23ed2ede651a6973e2fb22ea20fd3921932f23b4
pierce-06 <pierce.johan.cabalteja@student.ateneo.edu>
2026-03-01 23:29:21 +0800

    Added Commission model

--- commissions/models.py
+++ commissions/models.py
@@ -17,3 +17,20 @@ class CommissionType(models.Model):
         verbose_name = 'commission type'
         verbose_name_plural = 'commission types'
 
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    people_required = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('commissions:commission_detail', args=[str(self.pk)])
+    
+    class Meta:
+        ordering = ['created_on']
+        verbose_name = 'commission'
+        verbose_name_plural = 'commissions'
