c6da87da76f1ae6f48e141cd9d1a3520f039ad85
pierce-06 <pierce.johan.cabalteja@student.ateneo.edu>
2026-03-02 00:14:25 +0800

    Implemented admin panels

--- commissions/admin.py
+++ commissions/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class CommissionInline(admin.TabularInline):
+    model = Commission
+
+
+class CommissionTypeAdmin(admin.modelAdmin):
+    model = CommissionType
+    inlines = [CommissionInline, ]
+
+
+class CommissionAdmin(admin.modelAdmin):
+    model = Commission
+
+    search_fields = ('title', )
+
+    list_display = ('title', 'people_required')
+
+    list_filter = ('title', 'people_required')
+
+    fieldsets = [
+        ('Details', {
+            'fields': [
+                ('title', 'people_required')
+            ]
+        })
+    ]
+
+admin.site.register(CommissionType, CommissionTypeAdmin)
+admin.site.register(Commission, CommissionAdmin)
