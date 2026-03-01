027d3c203cf3ff24af20d450315eb067b8ceb98b
Mikey de los Reyes <erin.michaela.delosreyes@student.ateneo.edu>
2026-03-01 17:08:40 +0800

    Added Detail and List views. Templates TBA

--- localevents/views.py
+++ localevents/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import *
+
+
+class EventListView(ListView):
+    model = Event
+    template_name = #TEMPLATE TBA
+    
+
+class EventDetailView(DetailView):
+    model = Event
+    template_name = #TEMPLATE TBA
