b567e1b2f106dc9fcc5f1956c43a370b86287914
Mikey de los Reyes <erin.michaela.delosreyes@student.ateneo.edu>
2026-03-01 17:04:04 +0800

    Added Event model

--- localevents/models.py
+++ localevents/models.py
@@ -17,3 +17,27 @@ class EventType(models.Model):
         ordering = ['name']
         verbose_name = 'event type'
         verbose_name_plural = 'event types'
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(EventType, on_delete=models.SET_NULL,
+                                  related_name='events')
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('localevents:event_detail', args=[str(self.pk)])
+    
+
+    class Meta:
+        ordering = ['-created_on']
+        verbose_name = 'event'
+        verbose_name_plural = 'events'
