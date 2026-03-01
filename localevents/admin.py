f6e662b7eda744255abaa51bbac11d62cd93c1ec
Mikey de los Reyes <erin.michaela.delosreyes@student.ateneo.edu>
2026-03-01 21:04:05 +0800

    Added admin panel information

--- localevents/admin.py
+++ localevents/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class EventInLine(admin.TabularInline):
+    model = Event
+
+
+class EventTypeAdmin(admin.ModelAdmin):
+    model = EventType
+    inlines = [EventInLine,]
+
+
+class EventAdmin(admin.ModelAdmin):
+    model = Event
+
+    search_fields = ('title', )
+
+    list_display = ('title', 'location', 'start_time', 'end_time',)
+
+    list_filter = ('location', 'start_time', 'end_time',)  
+
+    fieldsets = [
+        ('Details', {
+            'fields': [
+                ('title', 'location', 'start_time', 'end_time'), 'category',
+            ]
+        }),
+    ]
+
+admin.site.register(EventType, EventTypeAdmin)
+admin.site.register(Event, EventAdmin)
