c67dbdeb7d3696a9cad18a44262eb805839b9bf1
pierce-06 <pierce.johan.cabalteja@student.ateneo.edu>
2026-03-01 22:48:50 +0800

    Added class based views for the List and Detail View

--- commissions/views.py
+++ commissions/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+class CommissionListView(ListView):
+    model = Commission
+    template_name = "commissions/commissions_list.html"
+
+class CommissionDetailView(DetailView):
+    model = Commission
+    template_name = "commissions/commissions_detail.html"
