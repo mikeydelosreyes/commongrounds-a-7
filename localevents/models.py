14eb90b77d90ef55a8b76d8e7fb2bb0a10ab70e1
Mikey de los Reyes <erin.michaela.delosreyes@student.ateneo.edu>
2026-03-01 15:28:57 +0800

    Added EventType model

--- localevents/models.py
+++ localevents/models.py
@@ -1,3 +1,19 @@
+from datetime import datetime
 from django.db import models
+from django.urls import *
 
-# Create your models here.
+class EventType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('eventtype_list', args=[str(self.name)])
+    
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'event type'
+        verbose_name_plural = 'event types'
