d69e9e3a2596208915b683c53b010c69d0832353
JonathanGelito <totojonathangelito@gmail.com>
2026-03-05 00:22:37 +0800

    added test.py

---
+++ bookclub/tests.py
@@ -0,0 +1,19 @@
+# Jonathan II A. Gelito
+# 242043
+# Febrauary 9, 2025
+
+# I have not discussed the Python language code in my program
+# with anyone other than my instructor or the teaching assistants
+# assigned to this course.
+
+# I have not used Python language code obtained from another student,
+# or any other unauthorized source, either modified or unmodified.
+
+# If any Python language code or documentation used in my program
+# was obtained from another source, such as a textbook or website,
+# that has been clearly noted with a proper citation in the comments
+# of my program.
+
+from django.test import TestCase
+
+# Create your tests here.
