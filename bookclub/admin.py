753eedbea94923f395dd6191ea8783192ef49c22
JonathanGelito <totojonathangelito@gmail.com>
2026-03-05 00:01:03 +0800

    added class GenreAdmin

--- bookclub/admin.py
+++ bookclub/admin.py
@@ -3,3 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+
+from .models import Profile 
+from .models import Genre, Book
+
+class GenreAdmin(admin.ModelAdmin):
+    model = Genre
