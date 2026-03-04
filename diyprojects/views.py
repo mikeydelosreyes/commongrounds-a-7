99c954be68da0f30626e678277927645b52778c0
Raphael Fernandez <raphael.tomines.fernandez@gmail>
2026-03-04 18:36:29 +0800

    updated views.py to add ListView and DetailView renamed app templates

--- diyprojects/views.py
+++ diyprojects/views.py
@@ -1,2 +1,13 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import ProjectCategory, Project
 # Create your views here.
+
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = 'diyprojects/project_list.html'
+
+class ProjectDetailView(DetailView):
+    model = Project
+    template_name = 'diyprojects/project_detail.html'
