7706661664078795200fe4af7b841a7355ae0d5e
Raphael Fernandez <raphael.tomines.fernandez@gmail>
2026-03-04 20:05:13 +0800

    added admin panel

--- diyprojects/admin.py
+++ diyprojects/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
-
+from .models import Project, ProjectCategory
 # Register your models here.
+
+class ProjectCategoryAdmin(admin.ModelAdmin):
+    model = ProjectCategory
+
+class ProjectAdmin(admin.ModelAdmin):
+    model = Project
+
+    search_fields = ('title', )
+
+    list_display = ('title', 'created_on', 'updated_on')
+
+    list_filter = ('created_on', 'updated_on')  
+
+    fieldsets = [
+        ('Details', {
+            'fields': [
+                ('title', 'created_on'), 'category',
+            ]
+        }),
+    ]
+
+admin.site.register(ProjectCategory, ProjectCategoryAdmin)
+admin.site.register(Project, ProjectAdmin)
+
+class ProjectInline(admin.TabularInline):
+    model = Project
+
+class ProjectCategoryAdmin(admin.ModelAdmin):
+    inlines = [ProjectInline]
