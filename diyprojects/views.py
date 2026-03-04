0e40ff8913b402b4092ad712de6637857347b218
Raphael Fernandez <raphael.tomines.fernandez@gmail>
2026-03-04 21:22:19 +0800

    temporary fix: added context_object_name in views.py

--- diyprojects/views.py
+++ diyprojects/views.py
@@ -7,7 +7,9 @@ from .models import ProjectCategory, Project
 class ProjectListView(ListView):
     model = Project
     template_name = 'diyprojects/project_list.html'
+    context_object_name = 'projects'
 
 class ProjectDetailView(DetailView):
     model = Project
     template_name = 'diyprojects/project_detail.html'
+    context_object_name = 'project'
