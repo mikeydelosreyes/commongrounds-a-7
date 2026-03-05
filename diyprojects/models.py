9191b7336262cb81bd9a1d901324e9aa23722862
Mikey de los Reyes <erin.michaela.delosreyes@student.ateneo.edu>
2026-03-05 21:10:58 +0800

    Fixed diyprojects models

--- diyprojects/models.py
+++ diyprojects/models.py
@@ -20,7 +20,7 @@ class ProjectCategory(models.Model):
 
 class Project(models.Model):
     title = models.CharField(max_length=255)
-    category = models.ForeignKey(ProjectCategory, on_delete=models.CASCADE)
+    category = models.ForeignKey(ProjectCategory, on_delete=models.SET_NULL)
     description = models.TextField()
     materials = models.TextField()
     steps = models.TextField()
