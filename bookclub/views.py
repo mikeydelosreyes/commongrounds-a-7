e4b347cf561ea62ec3e48bf213fc12f6b5097fb8
JonathanGelito <totojonathangelito@gmail.com>
2026-05-03 17:31:45 +0800

    Changed booklistview, added books_reviewed and books_reviewd but not books_contributed because there is not yet a role for book contributer yet

--- bookclub/views.py
+++ bookclub/views.py
@@ -20,16 +20,39 @@ def book_detail(request, id):
 class BookListView(ListView):
     model = Book
     template_name = "bookclub/book_list.html"
-    
+    context_object_name = "all_books"
+
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        book = self.object
 
         user=self.request.user
 
         if user.is_authenticated:
-            books_bookmarked = Bookmark.objects.filter(bookmark_profile=user)
-            books_bookreviewed = BookReview.objects.filter(UserReviewer=user)
+            
+            books_contributed = Book.objects
+
+
+            books_bookmarked = Book.objects.filter(
+                bookmarked_books__bookmark_profile=user
+            ).distinct()
+
+            books_reviewed = Book.objects.filter(
+                reviewed_books__UserReviewer=user
+            ).distinct()
+
+            grouped_books = (
+                books_bookmarked | books_reviewed
+            ).distinct()
+            
+            all_books = Book.objects.exclude(
+                id__in=grouped_books.values_list("id", flat=True)
+            )
+
+            context["contributed_books"] = books_contributed
+            context["bookmarked_books"] = books_bookmarked
+            context["reviewed_books"] = books_reviewed
+            context["all_books"] = all_books
 
 
         return context
