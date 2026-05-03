d5cd0f23880c174676deff5ea6d928092531c5c3
Mikey de los Reyes <erin.michaela.delosreyes@student.ateneo.edu>
2026-05-03 23:25:00 +0800

    Added create and update views

--- localevents/views.py
+++ localevents/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
 
 from .models import *
 
@@ -13,3 +15,17 @@ class EventListView(ListView):
 class EventDetailView(DetailView):
     model = Event
     template_name = "localevents/event_detail.html"
+
+class EventCreateView(LoginRequiredMixin, CreateView):
+    model = Event
+    form_class = EventForm
+
+
+    def form_valid(self, form):
+        form.instance.profile = Profile.objects.get(user=self.request.user)
+        return super().form_valid(form)
+
+class EventUpdateView(LoginRequiredMixin, UpdateView):
+    model = Event
+    form_class = EventForm
+    template_name = "localevents/event_update.html"
