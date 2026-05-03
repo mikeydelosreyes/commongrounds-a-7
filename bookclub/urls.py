aae8e1ed3fa466d68ece69daf48acf0ec94daecd
JonathanGelito <totojonathangelito@gmail.com>
2026-05-03 15:58:18 +0800

    changed BookListView and function book_detail

--- bookclub/urls.py
+++ bookclub/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("books", BooksListView.as_view(), name='books_list'),
+    path("books", ListAllView.as_view(), name='books_list'),
     path('book/<int:pk>/', BookDetailView.as_view(), name='books_detail'),
 
 ]
--- bookclub/views.py
+++ bookclub/views.py
@@ -1,10 +1,34 @@
 from .models import Book
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
-class BookListView(ListView):
+
+
+def book_detail(request, id):
+
+    Book = Book.objects.get(pk=id)
+
+    return render(request, 'ledger/recipe_detail.html', {
+        "book": Book,
+    })
+
+
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     template_name = "bookclub/book_list.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        book = self.object
+        context[""] = RecipeIngredient.objects.filter(
+            name_recipe=book
+        )
+        return context
+
 
 
 class BookDetailView(DetailView):
