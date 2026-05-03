0708867efd86465f041253e03390c930c2448fa7
pierce-06 <pierce.johan.cabalteja@student.ateneo.edu>
2026-05-03 17:25:56 +0800

    Added app-level URLs

--- commissions/urls.py
+++ commissions/urls.py
@@ -4,7 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('requests', CommissionListView.as_view(), name='commission_list'),
-    path('request/<int:pk>', CommissionDetailView.as_view(), name='commission_detail')
+    path('request/<int:pk>', CommissionDetailView.as_view(), name='commission_detail'),
+    path('request/add', CommissionCreateView.as_view(), name='commission_create'),
+    path('request/<int:pk>/edit', CommissionUpdateView.as_view(), name='commission_update')
 ]
 
 
