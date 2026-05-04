e562d43eaecdcf61b0b2e0fb825513e3dac5c252
Raphael Fernandez <raphael.tomines.fernandez@gmail>
2026-05-04 22:20:04 +0800

    Added CreateView in views.py

--- diyprojects/views.py
+++ diyprojects/views.py
@@ -1,7 +1,9 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import ProjectCategory, Project
+from django.views.generic.edit import CreateView, UpdateView
+from .models import *
+from .forms import *
 
 
 class ProjectListView(ListView):
@@ -13,3 +15,11 @@ class ProjectDetailView(DetailView):
     model = Project
     template_name = 'diyprojects/project_detail.html'
 
+class ProjectCreateView(CreateView):
+    model = Project
+    form_class = ProjectForm
+
+    def form_invalid(self, form):
+        form.instance.profile = Profile.objects.get(user=self.request.user)
+        return super().form_invalid(form)
+    
