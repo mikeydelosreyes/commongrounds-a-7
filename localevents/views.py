b141c4a33ba1d9de897e960291aad7d532903a19
Mikey de los Reyes <erin.michaela.delosreyes@student.ateneo.edu>
2026-05-04 15:53:29 +0800

    Updated views

--- localevents/views.py
+++ localevents/views.py
@@ -17,16 +17,22 @@ class EventDetailView(DetailView):
     model = Event
     template_name = "localevents/event_detail.html"
 
+
+#FIX LATER: https://stackoverflow.com/questions/18246326/how-do-i-set-user-field-in-form-to-the-currently-logged-in-user
 class EventCreateView(LoginRequiredMixin, CreateView):
     model = Event
     form_class = EventForm
-
+    exclude = ['organizer']
 
     def form_valid(self, form):
-        form.instance.profile = Profile.objects.get(user=self.request.user)
+        form.instance.organizer = Profile.objects.get(user=self.request.user)
         return super().form_valid(form)
+
 
 class EventUpdateView(LoginRequiredMixin, UpdateView):
     model = Event
     form_class = EventForm
+    exclude = ['organizer']
     template_name = "localevents/event_update.html"
+
+    #event capacity toggle here
