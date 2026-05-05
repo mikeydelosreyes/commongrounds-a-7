f60da87f225e45780df4bf850b8fc13cfca601e6
Raphael Fernandez <raphael.tomines.fernandez@gmail>
2026-05-05 19:28:54 +0800

    Added the display for all projects in views.py

--- diyprojects/views.py
+++ diyprojects/views.py
@@ -11,6 +11,11 @@ class ProjectListView(ListView):
     model = Project
     template_name = 'diyprojects/project_list.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        return context
+
 
 class ProjectDetailView(DetailView):
     model = Project
