bec4df9966d77e5e273a4735724574fc6b8d77ee
Raphael Fernandez <raphael.tomines.fernandez@gmail>
2026-05-06 20:33:26 +0800

    updated admin.py

--- diyprojects/admin.py
+++ diyprojects/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Project, ProjectCategory
+from .models import Project, ProjectCategory, ProjectRating, ProjectReview, Favorite
 
 
 class ProjectInline(admin.TabularInline):
@@ -11,24 +11,36 @@ class ProjectCategoryAdmin(admin.ModelAdmin):
     inlines = [ProjectInline]
 
 
-
 class ProjectAdmin(admin.ModelAdmin):
     model = Project
+    search_fields = ('title',)
 
-    search_fields = ('title', )
+    list_display = ('title', 'creator', 'category', 'created_on', 'updated_on')
 
-    list_display = ('title', 'created_on', 'updated_on')
-
-    list_filter = ('created_on', 'updated_on')  
-
+    list_filter = ('created_on', 'updated_on', 'category')
     fieldsets = [
         ('Details', {
             'fields': [
-                ('title', 'description', 'materials', 'steps'), 'category'
+                ('title', 'description', 'materials', 'steps'), 'category', 'creator'
             ]
         }),
     ]
 
 
+class ProjectRatingAdmin(admin.ModelAdmin):
+    model = ProjectRating
+    list_display = ('project', 'profile', 'score')
+    search_fields = ('project__title', 'profile__user__username')
+    list_filter = ('score',)
+
+
+class ProjectReviewAdmin(admin.ModelAdmin):
+    model = ProjectReview
+    list_display = ('project', 'reviewer', 'comment')
+    search_fields = ('project__title',)
+
+
 admin.site.register(ProjectCategory, ProjectCategoryAdmin)
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(ProjectRating, ProjectRatingAdmin)
+admin.site.register(ProjectReview, ProjectReviewAdmin)
