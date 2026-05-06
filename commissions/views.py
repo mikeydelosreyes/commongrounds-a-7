fa93355dc7be49f38143708197f8677a84fed4f2
pierce-06 <pierce.johan.cabalteja@student.ateneo.edu>
2026-05-06 21:38:40 +0800

    Updated the update view and and added an auto-sync functionality to the commission and job statuses

--- commissions/views.py
+++ commissions/views.py
@@ -28,7 +28,7 @@ class CommissionListView(ListView):
         ctx = super().get_context_data(**kwargs)
         qs = self.get_queryset()
         commissions = Commission.objects.all()
-
+        
         if self.request.user.is_authenticated:
             current_profile = Profile.objects.get(user=self.request.user)
             created_commissions = qs.filter(maker=current_profile)
@@ -52,6 +52,30 @@ class CommissionDetailView(DetailView):
         commission = self.object
 
         jobs = commission.jobs.all()
+
+        all_jobs_full = True
+
+        for job in jobs:
+            job.accepted_count = job.applications.filter(status='2_ACPT').count()
+            if job.accepted_count >= job.manpower_required:
+                if job.status != '2_FULL':
+                    job.status = '2_FULL'
+                    job.save()
+                else:
+                    all_jobs_full = False
+                    if job.status == "2_FULL":
+                        job.status == "1_OPEN"
+                        job.save()
+            
+        if not jobs.exclude(status='Full').exists():
+            if commission.status != 'Full': 
+                commission.status = 'Full' 
+                commission.save() 
+            elif not all_jobs_full: 
+                if commission.status == 'Full': 
+                    commission.status = 'Open' 
+                    commission.save() 
+        
         total_manpower = sum(job.manpower_required for job in jobs)
         accepted = sum(job.applications.filter(status='2_ACPT').count() for job in jobs) 
 
@@ -62,11 +86,7 @@ class CommissionDetailView(DetailView):
         ctx['open_manpower'] = max(0, open_manpower) 
 
         if self.request.user.is_authenticated:
-            ctx['current_profile'] = self.request.user.profile
-            ctx['job_apply_form'] = JobApplicationForm()
-            
-        for job in jobs:
-            job.accepted_count = job.applications.filter(status='2_ACPT').count()
+            ctx['current_profile'] = self.request.user.profile           
 
         ctx['jobs'] = jobs
 
@@ -83,11 +103,6 @@ class CommissionDetailView(DetailView):
         profile = request.user.profile
 
         if JobApplication.objects.filter(job=job, applicant=profile).exists():
-            return redirect('commissions:commission_detail', pk=self.object.pk)
-
-        accepted_count = job.applications.filter(status='2_ACPT').count()
-
-        if accepted_count >= job.manpower_required:
             return redirect('commissions:commission_detail', pk=self.object.pk)
 
         JobApplication.objects.create(
@@ -120,7 +135,7 @@ class CommissionCreateView(LoginRequiredMixin, CreateView):
             self.object = form.save()
             formset.instance = self.object
             formset.save()
-
+        
             return redirect('commissions:commission_detail', pk=self.object.pk)
         else:
             return self.render_to_response(self.get_context_data(form=form))
@@ -128,22 +143,31 @@ class CommissionCreateView(LoginRequiredMixin, CreateView):
 class CommissionUpdateView(LoginRequiredMixin, UpdateView):
     model = Commission
     form_class = CommissionForm
-    template_name = "commissions/commissions_update.html"    
+    template_name = "commissions/commissions_update.html"   
 
-    def form_valid(self, form):
-        response = super().form_valid(form)
-
-        commission = self.object
-        jobs = commission.jobs.all()
-
-        if all(job.status == '2_FULL' for job in jobs):
-            commission.status = 'Open'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.POST:
+            context['formset'] = JobFormSet(self.request.POST, instance=self.object)
         else:
-            commission.status = 'Full'
+            context['formset'] = JobFormSet(instance=self.object)
+        return context
+    
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['formset']
 
-        commission.save()
+        form.instance.maker = self.request.user.profile
+        
+        if formset.is_valid():
+            if not self.object.jobs.exclude(status='Full').exists():
+                self.object.status = "Full"
+            else:
+                self.object.status = "Open"
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
 
-        return response
-
-    def get_success_url(self):
-        return reverse('commissions:commission_detail', kwargs={'pk': self.object.pk})
+            return redirect('commissions:commission_detail', pk=self.object.pk)
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
