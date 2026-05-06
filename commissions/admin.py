3b5b645202fe52180ada749a06a7d20f98917027
pierce-06 <pierce.johan.cabalteja@student.ateneo.edu>
2026-05-06 16:55:41 +0800

    Added Panels for the Job and the JobApplication models

--- commissions/admin.py
+++ commissions/admin.py
@@ -23,6 +23,16 @@ class CommissionAdmin(admin.ModelAdmin):
         })
     ]
 
+class JobAdmin(admin.ModelAdmin):
+    model = Job
+    list_display = ('role', 'commission', 'manpower_required', 'status')
+    list_filter = ('status',)   
+
+
+class JobApplicationAdmin(admin.ModelAdmin):
+    model = JobApplication
+    list_display = ('applicant', 'job', 'status', 'applied_on')
+    list_filter = ('status',)
 
 admin.site.register(CommissionType)
 admin.site.register(Commission)
