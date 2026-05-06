64a4ea32d003fdd748fe5f2aa5dd3872f35d8a3b
JonathanGelito <totojonathangelito@gmail.com>
2026-05-06 16:57:02 +0800

    updated the book_detail and other urls

--- bookclub/urls.py
+++ bookclub/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("books", BookListView.as_view(), name='books_list'),
-    path('book/<int:pk>/', BookDetailView.as_view(), name='books_detail'),
-    path("book/add", BookCreateView.as_view(), name='books_create'),
-    path("book/<int:pk>/edit", BookUpdateView.as_view(), name='books_update'),
-    path("book/<int:pk>/borrow", BookBorrowView.as_view(), name='books_borrow'), 
+    path("books", BookListView.as_view(), name='book_list'),
+    path('book/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path("book/add", BookCreateView.as_view(), name='book_create'),
+    path("book/<int:pk>/edit", BookUpdateView.as_view(), name='book_update'),
+    path("book/<int:pk>/borrow", BookBorrowView.as_view(), name='book_borrow'), 
 ]
 
 app_name = "bookclub"
