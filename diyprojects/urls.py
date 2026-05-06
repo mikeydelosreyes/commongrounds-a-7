3dd8b7b7d9cc1eabff8459dad8180e0c4ffdbbb0
Raphael Fernandez <raphael.tomines.fernandez@gmail>
2026-05-06 17:46:42 +0800

    updated urls.py

--- diyprojects/urls.py
+++ diyprojects/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ProjectDetailView, ProjectListView
+from .views import *
 
 urlpatterns = [
     path('projects/', ProjectListView.as_view(), name='project_list'),
-    path('project/<int:pk>/', ProjectDetailView.as_view(), name='project_detail')
+    path('project/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
+    path('project/add/', ProjectCreateView.as_view(), name='project_create'),
+    path('project/<int:pk>/edit/', ProjectUpdateView.as_view(), name='project_update')
 ]
 
 app_name = "diyprojects"
