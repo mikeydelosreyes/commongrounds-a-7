8aa767c9b55b4af16e3af927a8e4486dc3d16299
Mikey de los Reyes <erin.michaela.delosreyes@student.ateneo.edu>
2026-05-07 17:05:38 +0800

    Added RoleRequiredMixin

--- accounts/models.py
+++ accounts/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from django.contrib.auth.models import *
 from django.urls import *
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=63)
     email = models.EmailField(max_length=254)
+    role = models.CharField(max_length=63, null=True)
 
     def __str__(self):
         return self.user.username
--- commongrounds/urls.py
+++ commongrounds/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls', namespace="accounts")),
     path('admin/', admin.site.urls),
     path('bookclub/', include('bookclub.urls', namespace="bookclub")),
     path('commissions/', include('commissions.urls', namespace="commissions")),
