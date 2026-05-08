07cbf3eb6de73cab59ee2fab63308cbb6e98c1fe
JonathanGelito <totojonathangelito@gmail.com>
2026-05-08 23:17:05 +0800

    This is mostly complete, will find bugs after deployment

--- commongrounds/urls.py
+++ commongrounds/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls', namespace="accounts")),
     path('admin/', admin.site.urls),
     path('bookclub/', include('bookclub.urls', namespace="bookclub")),
