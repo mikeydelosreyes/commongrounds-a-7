bbf7e865cbd3814d55e5c577f843896cdb718d88
JonathanGelito <totojonathangelito@gmail.com>
2026-05-09 00:47:26 +0800

    Redoing what I did last by renaming it back to username

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,4 +5,4 @@ from .models import *
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ["username"]
+        fields = ["name"]
--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import *
 from django.urls import *
-
+from django.template.defaultfilters import slugify
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length=63)
+    name = models.CharField(max_length=63)
     email = models.EmailField(max_length=254)
     role = models.CharField(max_length=63, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
 
+    @property
+    def slug(self):
+        return slugify(self.user.username)
