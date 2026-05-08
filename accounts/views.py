510f7f1a311496a0fdc0dac00fc3c93a2c2539ce
JonathanGelito <totojonathangelito@gmail.com>
2026-05-09 01:31:48 +0800

    Updated it. Now instead of using slugs, we are using get_or_create which utilizes the update form where it is not creating an object. I am also utilizing reverse_lazy so that I am not thinking or specifying that shitting thing.

--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,29 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, CreateView
 from .models import Profile
 from .forms import ProfileForm
-from django.template.defaultfilters import slugify
-
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
 
 class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     model = Profile
     form_class = ProfileForm
-    template_name = "accounts/displayname_update.html"
-
-    slug_field = "user__username"
-    slug_url_kwarg = "username"
+    template_name = "accounts/displayname_create.html"
 
     def get_object(self, queryset=None):
-        return Profile.objects.get(
-            user__username=self.kwargs["username"]
+        profile, _ = Profile.objects.get_or_create(
+        user=self.request.user,
+        defaults={"name": self.request.user.username, "email": self.request.user.email},
+    )
+        return profile
+
+    def get_success_url(self):
+        return reverse_lazy(
+            "accounts:profile_update", kwargs={"username": self.kwargs["username"]}
         )
+
+
+    
+
+
