a9aec313718b4fc1bc425158b13371a375246e11
Mikey de los Reyes <erin.michaela.delosreyes@student.ateneo.edu>
2026-05-09 20:30:31 +0800

    Fixed login bug

--- commongrounds/settings.py
+++ commongrounds/settings.py
@@ -131,5 +131,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_REDIRECT_URL=""
-LOGOUT_REDIRECT_URL="/accounts/login"
+LOGIN_URL = "/accounts/login"
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/accounts/login"
