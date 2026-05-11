fbaec38448479ea7aa308d97297e3b71f776f1ea
Mikey de los Reyes <erin.michaela.delosreyes@student.ateneo.edu>
2026-05-11 10:37:30 +0800

    Updated views

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import *
 from django.urls import *
 from django.template.defaultfilters import slugify
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=63)
